c757d8cf9fb58ac8d19b8a0b0e8a33412b7b8976
Masih Yeganeh <goodboy.php@gmail.com>
2024-07-04 14:35:46 +0330

    Updated to the last version of iso20022-client

--- go.mod
+++ go.mod
@@ -2,8 +2,7 @@ module github.com/CoreumFoundation/iso20022-addressbook
 
 go 1.21.4
 
-require (
-	// TODO: Replace with correct version when iso20022-client/iso20022 is released
-	github.com/CoreumFoundation/iso20022-client/iso20022 v0.0.0-20240621102656-b98b36321632
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0
-)
+// TODO: Replace with correct version when iso20022-client/iso20022 is released
+require github.com/CoreumFoundation/iso20022-client/iso20022 v0.0.0-20240704110255-e90ec955f83c
+
+require github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
--- validation_test.go
+++ validation_test.go
@@ -2,10 +2,8 @@ package iso20022_addressbook
 
 import (
 	"context"
-	"encoding/base64"
+	"fmt"
 	"testing"
-
-	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 
 	"github.com/CoreumFoundation/iso20022-client/iso20022/addressbook"
 )
@@ -15,61 +13,20 @@ func TestValidateAddresses(t *testing.T) {
 
 	chainIds := []string{"coreum-mainnet-1", "coreum-testnet-1", "coreum-devnet-1"}
 	for _, chainId := range chainIds {
-		ab := addressbook.NewWithRepoAddress(chainId, "file://./%s/addressbook.json")
+		ab := addressbook.NewWithRepoAddress(fmt.Sprintf("file://./%s/addressbook.json", chainId))
 
 		err := ab.Update(ctx)
 		if err != nil {
 			t.Fatalf("could not update %s address book: %v", chainId, err)
 		}
 
-		localAddressBook := make(map[string]struct{})
-		ab.ForEach(func(address addressbook.Address) bool {
-			if _, alreadyExists := localAddressBook[address.Bech32EncodedAddress]; alreadyExists {
-				t.Fatalf(
-					"duplicate entries with bech32 encoded address %q in %s",
-					address.Bech32EncodedAddress,
-					chainId,
-				)
-			}
-
-			publicKeyBytes, err := base64.StdEncoding.DecodeString(address.PublicKey)
-			if err != nil {
-				t.Fatalf(
-					"public key of %q is not a valid base64 encoded string in %s: %v",
-					address.Bech32EncodedAddress,
-					chainId,
-					err,
-				)
-			}
-
-			if _, err = secp256k1.ParsePubKey(publicKeyBytes); err != nil {
-				t.Fatalf(
-					"public key of %q is not a valid secp256k1 public key in %s: %v",
-					address.Bech32EncodedAddress,
-					chainId,
-					err,
-				)
-			}
-			localAddressBook[address.Bech32EncodedAddress] = struct{}{}
-			matches := make([]string, 0, 1)
-			ab.ForEach(func(address2 addressbook.Address) bool {
-				if address.BranchAndIdentification.Equal(address2.BranchAndIdentification) {
-					matches = append(matches, address2.Bech32EncodedAddress)
-					if len(matches) > 1 {
-						return false
-					}
-				}
-				return true
-			})
-			if len(matches) > 1 {
-				t.Fatalf(
-					"ISO20022 branch and identification data of entry %q and %q conflicts in %s.",
-					matches[0],
-					matches[1],
-					chainId,
-				)
-			}
-			return true
-		})
+		err = ab.Validate()
+		if err != nil {
+			t.Fatalf(
+				"%s addressbook error: %v",
+				chainId,
+				err,
+			)
+		}
 	}
 }
